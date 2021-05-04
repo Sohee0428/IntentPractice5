40217242530953628ca9418b2a16fd45ff52f22e
Sohee <sohee3480@gmail.com>
2021-05-04 20:02:02 +0900

    DAIL 액션 예제 코드 구현

--- app/src/main/java/com/example/intentpractice5/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice5/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.intentpractice5
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        DIAL 액션 예제
+
+        dialBnt.setOnClickListener {
+
+//            phoneNumEdt에 입력한 전화번호를 받아서 => 해당 번호에 연결
+
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+
+            val myUri = Uri.parse("tel : ${inputPhoneNum}")
+
+
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+
+            startActivity(myIntent)
+        }
     }
 }
